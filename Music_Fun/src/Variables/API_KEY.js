358ef773695f7e49fde89513978589019345afcb
Papu Kachari <kacharipapu54@gmail.com>
2026-09-08 21:47:33 +0530

    api

--- Music_Fun/src/Variables/API_KEY.js
+++ Music_Fun/src/Variables/API_KEY.js
@@ -1,3 +1,3 @@
 export const API_KEY = import.meta.env.VITE_YOUTUBE_API_KEY
-export const API_URL = 'https://www.googleapis.com/youtube/v3/search'const API_KEY = "AIzaSyAwO2eSpS6OIMqpc8d4eZGdz1mnQ8aF3kU"; // Replace with your actual API key
-const API_URL = "https://www.googleapis.com/youtube/v3/search";
+export const API_URL = 'https://www.googleapis.com/youtube/v3/search'
+export const VIDEOS_API_URL = 'https://www.googleapis.com/youtube/v3/videos'
